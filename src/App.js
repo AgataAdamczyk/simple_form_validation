b208bc45d1e908900c5c209c9af8cbdda855da52
Agata Adamczyk <agata.bodakiewicz@wp.pl>
2019-11-26 15:17:39 +0100

    form with validation

---
+++ src/App.js
@@ -0,0 +1,162 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    accept: false,
+    message: '',
+
+    errors: {
+      username: false,
+      email: false,
+      password: false,
+      accept: false,
+    }
+  }
+
+  messages = {
+    username_incorrect: 'Imię jest za krótkie',
+    email_incorrect: 'Nieprawidłowy email',
+    password_incorrect: 'Hasło musi mieć min. 8 znaków',
+    accept_incorrect: 'Zgoda wymagana',
+  }
+
+  handleChange = (e) => {
+
+    const name = e.target.name;
+    const type = e.target.type;
+
+    if(type === "text" || type === "password" || type === "email") {
+      const value = e.target.value;
+      this.setState({
+        [name]: value
+      })
+    } else if (type === "checkbox") {
+      const checked = e.target.checked;
+      this.setState({
+        [name]: checked
+      })
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const validation = this.formValidation()
+
+    if(validation.correct) {
+      this.setState({
+        username: '',
+        email: '',
+        password: '',
+        accept: false,
+        message: 'Formularz został wysłany',
+
+        errors: {
+          username: false,
+          email: false,
+          password: false,
+          accept: false,
+        }
+      })
+    } else {
+      this.setState({
+        errors: {
+          username: !validation.username,
+          email: !validation.email,
+          password: !validation.password,
+          accept: !validation.accept,
+        }
+      })
+    }
+  }
+
+  formValidation = () => {
+    //true - ok
+    //false - źle
+    let username = false;
+    let email = false;
+    let password = false;
+    let accept = false;
+    let correct = false;
+    
+    if(this.state.username.length >= 3 && this.state.username.indexOf(' ') === -1) {
+      username = true;
+    }
+
+    if(this.state.email.indexOf('@') !== -1) {
+      email = true;
+    }
+
+    if(this.state.password.length === 8) {
+      password = true;
+    }
+
+    if(this.state.accept) {
+      accept = true;
+    }
+
+    if(username && email && password && accept) {
+      correct = true;
+    }
+
+    return ({
+      correct,
+      username,
+      email,
+      password,
+      accept,
+    })
+  }
+
+  componentDidUpdate() {
+    if(this.state.message !== '') {
+      setTimeout(() => this.setState({
+        message: ''
+      }), 3000)
+    }
+  }
+
+  render() {
+    return (
+      <div className="form">
+        <form onSubmit={this.handleSubmit} noValidate>
+          <label htmlFor="user">Imię: 
+            <input type="text" id="user" name="username"
+            value={this.state.username} onChange={this.handleChange}/>
+          </label>
+            {this.state.errors.username && <span>{this.messages.username_incorrect}</span>}
+
+          <label htmlFor="email">Email:
+          <input type="email" id="email" name="email"
+            value={this.state.email} onChange={this.handleChange}/>
+          </label>
+            {this.state.errors.email && <span>{this.messages.email_incorrect}</span>}
+
+          <label htmlFor="password">Hasło: 
+            <input type="password" id="password" name="password"
+            value={this.state.password} onChange={this.handleChange}/>
+          </label>
+            {this.state.errors.password && <span>{this.messages.password_incorrect}</span>}
+
+          <label className="checkbox" htmlFor="accept">
+            <input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleSubmit} />
+            Zgoda na przetważanie danych
+          </label>
+            {this.state.errors.accept && <span>{this.messages.accept_incorrect}</span>}
+
+          <button className="button">Zapisz się</button>
+
+        </form>
+          {this.state.message && <h3>{this.state.message}</h3>}
+      </div>
+    )
+  }
+
+}
+
+export default App;
